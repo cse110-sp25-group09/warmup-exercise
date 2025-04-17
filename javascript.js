78668d3e3f21ac5799201b347da7dc35680b95b1
Travis Henry <travman.henry1@gmail.com>
2025-04-17 14:08:27 -0700

    added function outcomes

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//four functions needed
+//shuffle deck: card to flip is randomized
+//take: takes card from card to flip, reshuffles new card, remove taken from list
+//discard: remove card from deck, do not take, reshuffle
+//reset: refill list and reshuffle and clear hand
+
+
